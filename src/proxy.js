08ced714fee1fcd14f02ce0832e1b3b9f18cd9e2
sumku404 <sumku55@yahoo.com>
2021-08-21 17:13:31 +0800

    new strategy for routing url

---
+++ src/proxy.js
@@ -0,0 +1,18 @@
+// Proxy by sumkux 2021
+
+let toggle = false;
+
+let routeMapping = [];
+
+function mount() {
+	toggle = true;
+}
+
+function unmount() {
+	toggle = false;
+	routeMapping = [];
+}
+
+function route(destination, url) {
+	// do somthing...
+}
