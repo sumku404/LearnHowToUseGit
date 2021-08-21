d38359e6773faeb6a7b9e308960930a7bfcd39e4
sumku404 <sumku55@yahoo.com>
2021-08-21 17:09:11 +0800

    created a test js file in src/

---
+++ src/function.js
@@ -0,0 +1,21 @@
+let _SYSTEM  = "Unix"
+
+let _VERSION = "1.0.0"
+
+let _AUTHOR  = "sumkux"
+
+(function(args) {
+
+
+	let cpyData = function(data, target) {
+		if(date == '') return;
+
+		//...do something
+	}
+})(
+	{
+		system  : _SYSTEM,
+		version : _VERSION,
+		author  : _AUTHOR
+	}
+)
